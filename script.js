ed817d8fde8d5af0e7ebfa10bf90719faf7164d5
Guilherme Clara de Carvalho <121357323+Guilhermen33@users.noreply.github.com>
2023-08-22 11:43:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -50,12 +50,10 @@ if(window.open){
         document.getElementById('dois').style.display='none'
         document.getElementById('treis').style.display='none'
         document.getElementById('quatro').style.display='none'
-        document.querySelector('.container-dicas').style.height='400px'
         document.body.style.height='3000px'
         document.getElementById('video').volume ='0'
-        document.querySelector('footer').style.marginTop='0'
         document.querySelector('.dicas').style.height='380px'
-
+        document.querySelector('footer').style.marginTop='0'
             var inputsDicas =  document.querySelectorAll('.inptut-dicas')
         inputsDicas.forEach(element => {
             element.addEventListener("click" , () =>{
@@ -70,6 +68,9 @@ if(window.open){
                 else if(element.value === '4' ){
                     document.getElementById("titulo-dica").innerHTML= 'Economize Tinta Quando Possível:'
                     document.getElementById("texto-dica").innerHTML= 'Para economizar tinta, use a configuração de impressão de rascunho quando a qualidade máxima não for necessária. Além disso, considere usar fontes que usam menos tinta e evite imprimir páginas com grandes áreas sólidas de cores, pois isso pode esgotar a tinta rapidamente.'
+                }else{
+                    document.getElementById("titulo-dica").innerHTML= 'Armazenamento Adequado:'
+                    document.getElementById("texto-dica").innerHTML= 'Mantenha suas tintas de impressora em um local fresco e seco, longe da luz direta do sol e de temperaturas extremas. A exposição a condições adversas pode afetar a qualidade da tinta e sua capacidade de imprimir corretamente.'
                 }
             })
         });
@@ -80,6 +81,11 @@ if(window.open){
         document.getElementById('dois').style.display='flex'
         document.getElementById('treis').style.display='flex'
         document.getElementById('quatro').style.display='flex'
+        document.querySelector('.container-dicas').style.height='100%'
+        document.body.style.height='100vh'
+        document.getElementById('video').volume ='0'
+    
+        document.querySelector('.dicas').style.height='700px'
     }
 }
 
@@ -90,19 +96,24 @@ function verificarTamanhoDaTela(){
         document.getElementById('dois').style.display='none'
         document.getElementById('treis').style.display='none'
         document.getElementById('quatro').style.display='none'
-        document.querySelector('.container-dicas').style.height='400px'
-        document.querySelector('.dicas').style.height='400px'
         document.querySelector('footer').style.marginTop='0'
+        document.querySelector('.dicas').style.height='400px'
         document.body.style.height='3000px'
-        document.getElementById('video').volume ='0'
+        document.getElementById('video').volume ='0px'
     }
     else{
         document.querySelector('.inputs-dicas').style.display='none'
         document.getElementById('dois').style.display='flex'
         document.getElementById('treis').style.display='flex'
         document.getElementById('quatro').style.display='flex'
+        document.querySelector('.container-dicas').style.height='100%'
+        document.body.style.height='100%'
+        document.getElementById('video').volume ='0'
+       
+        document.querySelector('.dicas').style.height='700px'
     }
 }
 
 
 window.addEventListener("resize", verificarTamanhoDaTela);
+
