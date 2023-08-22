ae5e135cd85d8fdeca61fd74ec5406bca0d07aca
Guilherme Clara de Carvalho <121357323+Guilhermen33@users.noreply.github.com>
2023-08-22 17:27:39 -0300

    melhorei para celular

--- script.js
+++ script.js
@@ -29,106 +29,29 @@ box.forEach(element => {
 })
 
 
-if (window.open) {
-    var tamanhoTela = window.innerWidth;
-    if (tamanhoTela <= 500) {
-        document.querySelector('.inputs-dicas').style.display = 'block'
-        document.querySelector('.inputs-dicas').style.marginTop = '1em'
-        document.getElementById('dois').style.display = 'none'
-        document.getElementById('treis').style.display = 'none'
-        document.getElementById('quatro').style.display = 'none'
-        document.body.style.height = '3000px'
+var inputsDicas = document.querySelectorAll('.inptut-dicas')
+inputsDicas.forEach(element => {
+    element.addEventListener("click", () => {
 
-        document.querySelector('.dicas').style.height = '380px'
-        document.querySelector('footer').style.marginTop = '0'
-        var inputsDicas = document.querySelectorAll('.inptut-dicas')
-        inputsDicas.forEach(element => {
-            element.addEventListener("click", () => {
-
-                if (element.value === '1') {
-                    document.getElementById("titulo-dica").innerHTML = 'Armazenamento Adequado:'
-                    document.getElementById("texto-dica").innerHTML = 'Mantenha suas tintas de impressora em um local fresco e seco, longe da luz direta do sol e de temperaturas extremas. A exposição a condições adversas pode afetar a qualidade da tinta e sua capacidade de imprimir corretamente.'
-                }
-                else if (element.value === '2') {
-                    document.getElementById("titulo-dica").innerHTML = 'Agite Antes de Usar:'
-                    document.getElementById("texto-dica").innerHTML = 'Antes de inserir um novo cartucho de tinta na impressora, agite-o suavemente. Isso ajuda a garantir que os pigmentos ou tintas líquidas estejam bem misturados e evita problemas de impressão, como cores desiguais.'
-                }
-                else if (element.value === '3') {
-                    document.getElementById("titulo-dica").innerHTML = 'Imprima Regularmente:'
-                    document.getElementById("texto-dica").innerHTML = 'Se você não imprime com frequência, pode ser útil fazer impressões regulares para manter o fluxo de tinta e evitar que os cartuchos sequem. '
-                }
-                else if (element.value === '4') {
-                    document.getElementById("titulo-dica").innerHTML = 'Economize Tinta Quando Possível:'
-                    document.getElementById("texto-dica").innerHTML = 'Para economizar tinta, use a configuração de impressão de rascunho quando a qualidade máxima não for necessária. Além disso, considere usar fontes que usam menos tinta e evite imprimir páginas com grandes áreas sólidas de cores, pois isso pode esgotar a tinta rapidamente.'
-                }
-            })
-        });
-
-    }
-    else {
-        document.querySelector('.inputs-dicas').style.display = 'none'
-        document.getElementById('dois').style.display = 'flex'
-        document.getElementById('treis').style.display = 'flex'
-        document.getElementById('quatro').style.display = 'flex'
-        document.querySelector('.container-dicas').style.height = '100%'
-        document.body.style.height = '100vh'
-        document.querySelector("footer").style.marginTop= '5em'
-
-
-        document.querySelector('.dicas').style.height = '700px'
-    }
-}
-
-function verificarTamanhoDaTela() {
-    var tamanhoTela = window.innerWidth;
-    if (tamanhoTela <= 500) {
-        document.querySelector('.inputs-dicas').style.display = 'block'
-        document.getElementById('dois').style.display = 'none'
-        document.getElementById('treis').style.display = 'none'
-        document.getElementById('quatro').style.display = 'none'
-        document.querySelector('footer').style.marginTop = '0'
-        document.querySelector('.dicas').style.height = '400px'
-        document.body.style.height = '3000px'
-        var inputsDicas = document.querySelectorAll('.inptut-dicas')
-        inputsDicas.forEach(element => {
-            element.addEventListener("click", () => {
-
-                if (element.value === '1') {
-                    document.getElementById("titulo-dica").innerHTML = 'Armazenamento Adequado:'
-                    document.getElementById("texto-dica").innerHTML = 'Mantenha suas tintas de impressora em um local fresco e seco, longe da luz direta do sol e de temperaturas extremas. A exposição a condições adversas pode afetar a qualidade da tinta e sua capacidade de imprimir corretamente.'
-                }
-                else if (element.value === '2') {
-                    document.getElementById("titulo-dica").innerHTML = 'Agite Antes de Usar:'
-                    document.getElementById("texto-dica").innerHTML = 'Antes de inserir um novo cartucho de tinta na impressora, agite-o suavemente. Isso ajuda a garantir que os pigmentos ou tintas líquidas estejam bem misturados e evita problemas de impressão, como cores desiguais.'
-                }
-                else if (element.value === '3') {
-                    document.getElementById("titulo-dica").innerHTML = 'Imprima Regularmente:'
-                    document.getElementById("texto-dica").innerHTML = 'Se você não imprime com frequência, pode ser útil fazer impressões regulares para manter o fluxo de tinta e evitar que os cartuchos sequem. '
-                }
-                else if (element.value === '4') {
-                    document.getElementById("titulo-dica").innerHTML = 'Economize Tinta Quando Possível:'
-                    document.getElementById("texto-dica").innerHTML = 'Para economizar tinta, use a configuração de impressão de rascunho quando a qualidade máxima não for necessária. Além disso, considere usar fontes que usam menos tinta e evite imprimir páginas com grandes áreas sólidas de cores, pois isso pode esgotar a tinta rapidamente.'
-                }
-            })
-        });
-
-    }
-    else {
-        document.querySelector('.inputs-dicas').style.display = 'none'
-        document.getElementById('dois').style.display = 'flex'
-        document.getElementById('treis').style.display = 'flex'
-        document.getElementById('quatro').style.display = 'flex'
-        document.querySelector('.container-dicas').style.height = '100%'
-        document.body.style.height = '100%'
-
-
-        document.querySelector('.dicas').style.height = '700px'
-        document.querySelector("footer").style.marginTop= '11em'
-
-    }
-}
+        if (element.value === '1') {
+            document.getElementById("titulo-dica").innerHTML = 'Armazenamento Adequado:'
+            document.getElementById("texto-dica").innerHTML = 'Mantenha suas tintas de impressora em um local fresco e seco, longe da luz direta do sol e de temperaturas extremas. A exposição a condições adversas pode afetar a qualidade da tinta e sua capacidade de imprimir corretamente.'
+        }
+        else if (element.value === '2') {
+            document.getElementById("titulo-dica").innerHTML = 'Agite Antes de Usar:'
+            document.getElementById("texto-dica").innerHTML = 'Antes de inserir um novo cartucho de tinta na impressora, agite-o suavemente. Isso ajuda a garantir que os pigmentos ou tintas líquidas estejam bem misturados e evita problemas de impressão, como cores desiguais.'
+        }
+        else if (element.value === '3') {
+            document.getElementById("titulo-dica").innerHTML = 'Imprima Regularmente:'
+            document.getElementById("texto-dica").innerHTML = 'Se você não imprime com frequência, pode ser útil fazer impressões regulares para manter o fluxo de tinta e evitar que os cartuchos sequem. '
+        }
+        else if (element.value === '4') {
+            document.getElementById("titulo-dica").innerHTML = 'Economize Tinta Quando Possível:'
+            document.getElementById("texto-dica").innerHTML = 'Para economizar tinta, use a configuração de impressão de rascunho quando a qualidade máxima não for necessária. Além disso, considere usar fontes que usam menos tinta e evite imprimir páginas com grandes áreas sólidas de cores, pois isso pode esgotar a tinta rapidamente.'
+        }
+    })
+});
 
 
 window.addEventListener("resize", verificarTamanhoDaTela);
 
-document.querySelector("footer").style.marginTop= '11em'
